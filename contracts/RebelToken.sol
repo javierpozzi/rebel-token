c465a555cd949649322291ee020e8a9b0e951840
Javier Pozzi <javierpozzi@gmail.com>
2021-12-06 00:23:14 -0300

    Migrate Solidity to ^0.6.0

--- contracts/RebelToken.sol
+++ contracts/RebelToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract REBToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Rebel Token", "REB") {
+    constructor(uint256 initialSupply) ERC20("Rebel Token", "REB") public {
         _mint(msg.sender, initialSupply);
     }
 }
